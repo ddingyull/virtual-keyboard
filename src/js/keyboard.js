29ca38d1eceb67ad14a407cc112eb8e7bf0a51b1
yurim <dding.yull@gmail.com>
2022-12-20 19:19:46 +0900

    feat: font수정 기능 구현

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -1,11 +1,13 @@
 export class Keyboard {
   #switchEl; //#를 붙인 값은 class 외부에서 다른 값을 덮어씌울 수 없게 함
+  #fontSelectEl;
   constructor() {
     this.#assignElement(); //요소 가져오고
     this.#addEvent(); //이벤트 붙여줬고 constructor 실행먼저 해줌
   }
   #assignElement() {
     this.#switchEl = document.getElementById("switch");
+    this.#fontSelectEl = document.getElementById("font");
   }
 
   #addEvent() {
@@ -14,6 +16,9 @@ export class Keyboard {
         "theme",
         e.target.checked ? "dark-mode" : ""
       );
+    });
+    this.#fontSelectEl.addEventListener("change", (e) => {
+      document.body.style.fontFamily = e.target.value;
     });
   }
 }
