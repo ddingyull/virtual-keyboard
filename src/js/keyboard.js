09efe1f3156129b192d041d9fb05a574ead38df9
yurim <dding.yull@gmail.com>
2023-01-05 15:55:03 +0900

    feat(keyboard):자판눌리는 기능 구현

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -2,6 +2,7 @@ export class Keyboard {
   #switchEl; //#를 붙인 값은 class 외부에서 다른 값을 덮어씌울 수 없게 함
   #fontSelectEl;
   #containerEl;
+  #keyboardEl;
   constructor() {
     this.#assignElement(); //요소 가져오고
     this.#addEvent(); //이벤트 붙여줬고 constructor 실행먼저 해줌
@@ -10,6 +11,7 @@ export class Keyboard {
     this.#containerEl = document.getElementById("container");
     this.#switchEl = this.#containerEl.querySelector("#switch");
     this.#fontSelectEl = this.#containerEl.querySelector("#font");
+    this.#keyboardEl = this.#containerEl.querySelector("#keyboard");
   }
 
   #addEvent() {
@@ -17,6 +19,17 @@ export class Keyboard {
     this.#switchEl.addEventListener("change", this.#onChangeTheme);
     // 폰트변경 기능
     this.#fontSelectEl.addEventListener("change", this.#onChangeFont);
+    // 키보드 누르면 작성되는 기능
+    document.addEventListener("keydown", (e) => {
+      this.#keyboardEl
+        .querySelector(`[data-code=${e.code}]`)
+        ?.classList.add("active");
+    });
+    document.addEventListener("keyup", (e) => {
+      this.#keyboardEl
+        .querySelector(`[data-code=${e.code}]`)
+        ?.classList.remove("active");
+    });
   }
 
   #onChangeTheme(e) {
