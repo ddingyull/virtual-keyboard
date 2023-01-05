8131b740a58b990c7baf4ed709bf4075b93cfc25
yurim <dding.yull@gmail.com>
2023-01-05 14:23:20 +0900

    refactory(keyboard):다크모드, 폰트변경 기능 리팩토링

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -1,26 +1,32 @@
 export class Keyboard {
   #switchEl; //#를 붙인 값은 class 외부에서 다른 값을 덮어씌울 수 없게 함
   #fontSelectEl;
+  #containerEl;
   constructor() {
     this.#assignElement(); //요소 가져오고
     this.#addEvent(); //이벤트 붙여줬고 constructor 실행먼저 해줌
   }
   #assignElement() {
-    this.#switchEl = document.getElementById("switch");
-    this.#fontSelectEl = document.getElementById("font");
+    this.#containerEl = document.getElementById("container");
+    this.#switchEl = this.#containerEl.querySelector("#switch");
+    this.#fontSelectEl = this.#containerEl.querySelector("#font");
   }
 
   #addEvent() {
     // 다크모드 기능
-    this.#switchEl.addEventListener("change", (e) => {
-      document.documentElement.setAttribute(
-        "theme",
-        e.target.checked ? "dark-mode" : ""
-      );
-    });
+    this.#switchEl.addEventListener("change", this.#onChangeTheme);
     // 폰트변경 기능
-    this.#fontSelectEl.addEventListener("change", (e) => {
-      document.body.style.fontFamily = e.target.value;
-    });
+    this.#fontSelectEl.addEventListener("change", this.#onChangeFont);
+  }
+
+  #onChangeTheme(e) {
+    document.documentElement.setAttribute(
+      "theme",
+      e.target.checked ? "dark-mode" : ""
+    );
+  }
+
+  #onChangeFont(e) {
+    document.body.style.fontFamily = e.target.value;
   }
 }
